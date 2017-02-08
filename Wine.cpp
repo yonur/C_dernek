caea8fa5a844e75f42b5ca5b6eb1ccbda85ed011
yonur <yonurpirdal@hotmail.com>
2017-02-08 20:08:03 +0200

    Update Wine.cpp

--- Wine.cpp
+++ Wine.cpp
@@ -2,25 +2,32 @@
 
 Wine::Wine()
 {
-}
 
+}
 
 Wine::~Wine()
 {
+
 }
 
 template<class T1, class T2>
 T1 & Pair<T1, T2>::first()
 {
+
 	return a;
+
 }
+
 template<class T1, class T2>
 T2 & Pair<T1, T2>::second()
 {
+
 	return b;
+
 }
 
 std::string & Wine::Label() {
-	std::cout << "Sarap markasini giriniz: " << std::endl;
+
+	return this->label;
 
 }
